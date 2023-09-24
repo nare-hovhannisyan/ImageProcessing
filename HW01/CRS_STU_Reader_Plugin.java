47046f73615711c6df548b0490ded2b9e2d2b671
Nare Hovhannisyan <nare.hovhannisyan98@gmail.com>
2023-09-24 23:46:27 +0400

    fixed_dataset

--- HW01/CRS_STU_Reader_Plugin.java
+++ HW01/CRS_STU_Reader_Plugin.java
@@ -11,12 +11,19 @@ import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CRS_STU_Reader_Plugin implements PlugIn {
 
     public void run(String arg) {
 
-        String crsFilePath = "/Users/narehovhannisyan/Desktop/classes/Image Processing/Github/ImageProcessing/HW01/tre-s-92.crs";
-        String stuFilePath = "/Users/narehovhannisyan/Desktop/classes/Image Processing/Github/ImageProcessing/HW01/tre-s-92.stu";
+        String crsFilePath = "/Users/narehovhannisyan/Desktop/classes/Image Processing/Github/ImageProcessing/HW01/lse-f-91.crs";
+        String stuFilePath = "/Users/narehovhannisyan/Desktop/classes/Image Processing/Github/ImageProcessing/HW01/lse-f-91.stu";
 
         int N = 0;
 
@@ -47,18 +54,19 @@ public class CRS_STU_Reader_Plugin implements PlugIn {
         byteProcessor.setValue(255);
         byteProcessor.fill();
 
+        List<int[]> coordinatePairs = new ArrayList<>();
         try {
+
             BufferedReader stuReader = new BufferedReader(new FileReader(stuFilePath));
 
-            String stuLine;
-            Pattern stuPattern = Pattern.compile("(\\d+) (\\d+)");
-            while ((stuLine = stuReader.readLine()) != null) {
-                Matcher stuMatcher = stuPattern.matcher(stuLine);
-                while (stuMatcher.find()) {
-                    int x = Integer.parseInt(stuMatcher.group(1));
-                    int y = Integer.parseInt(stuMatcher.group(2));
-                    if (x >= 0 && x < N && y >= 0 && y < N) {
-                        byteProcessor.putPixel(x, y, 0);
+            String line;
+            while ((line = stuReader.readLine()) != null) {
+                String[] tokens = line.trim().split("\\s+");
+                if (tokens.length >= 2) {
+                    for (int i = 0; i < tokens.length - 1; i += 2) {
+                        int x = Integer.parseInt(tokens[i]);
+                        int y = Integer.parseInt(tokens[i + 1]);
+                        coordinatePairs.add(new int[] { x, y });
                     }
                 }
             }
@@ -67,6 +75,15 @@ public class CRS_STU_Reader_Plugin implements PlugIn {
         } catch (IOException e) {
             IJ.error("Error reading the .stu file: " + e.getMessage());
             return;
+        }
+
+        for (int[] pair : coordinatePairs) {
+            int x = pair[0];
+            int y = pair[1];
+            if (x >= 0 && x < N && y >= 0 && y < N) {
+                byteProcessor.set(x, y, 0);
+            }
+
         }
 
         ImagePlus binaryImage = new ImagePlus("Binary Image", byteProcessor);
